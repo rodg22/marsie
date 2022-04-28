b74cc20297e659a890907f045124f15728a284df
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2022-04-27 21:30:10 -0300

    Prueba 1 - Caja modal team members

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,14 @@
 const d = document;
-const $hamburger = document.getElementById("hamburger");
-const $main = document.querySelector("main");
-const $hamburgerClass = document.querySelector(".hamburger");
-const $menuClass = document.querySelector(".menu");
-const $linkMenu = document.querySelectorAll(".menu ul li a");
-const $circleMenu = document.getElementById("menu-bg");
-const $blurMenu = document.getElementById("menu-blur");
+const $hamburger = d.getElementById("hamburger");
+const $main = d.querySelector("main");
+const $hamburgerClass = d.querySelector(".hamburger");
+const $menuClass = d.querySelector(".menu");
+const $linkMenu = d.querySelectorAll(".menu ul li a");
+const $circleMenu = d.getElementById("menu-bg");
+const $blurMenu = d.getElementById("menu-blur");
+const $teamMembers = d.querySelectorAll(".team-member");
+const $modalMember = d.querySelectorAll(".team-modal-child");
+const $modalArrows = d.querySelectorAll(".arrow-out-modal");
 
 d.addEventListener("DOMContentLoaded", () => {
   $hamburger.addEventListener("click", () => {
@@ -15,14 +18,36 @@ d.addEventListener("DOMContentLoaded", () => {
     $blurMenu.classList.toggle("change-to-blured");
   });
 
-  $linkMenu.forEach((e) =>
-    e.addEventListener("click", () => {
+  $linkMenu.forEach((link) =>
+    link.addEventListener("click", () => {
       $hamburgerClass.classList.toggle("open");
       $menuClass.classList.toggle("change");
       $circleMenu.classList.toggle("change-bg");
       $blurMenu.classList.toggle("change-to-blured");
     })
   );
+
+  $teamMembers.forEach((link) =>
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      $modalMember.forEach((member) => {
+        const miembroSlider = link.querySelector("span").innerText;
+        const miembroModal = member.querySelector("h4").innerText;
+        if (miembroModal === miembroSlider) {
+          // alert(`El miembro seleccionado es ${miembroModal}`);
+          member.classList.add("visible-member");
+        }
+      });
+    })
+  );
+
+  $modalArrows.forEach((arrow) => {
+    arrow.addEventListener("click", () => {
+      $modalMember.forEach((member) => {
+        member.classList.remove("visible-member");
+      });
+    });
+  });
 });
 
 // --------------------- SWIPER FOUNDERS ----------------------
@@ -40,6 +65,8 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// ---------------------- SWIPER VALUES -----------------------
 
 var swiper = new Swiper(".valuesSwiper", {
   spaceBetween: 0,
