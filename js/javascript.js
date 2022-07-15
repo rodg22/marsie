1b5ae02f8fd0d43a71876ecf6d0ed93794fa550a
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2022-07-15 16:59:17 -0300

    Corregido el scrollTo del boton scrollTop

--- js/javascript.js
+++ js/javascript.js
@@ -312,7 +312,11 @@ d.addEventListener("DOMContentLoaded", () => {
 
   $scrollTop.addEventListener("click", (e) => {
     e.preventDefault();
-    scroll(0, 0);
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   });
   d.addEventListener("scroll", () => {
     if (scrollY > 1000) {
