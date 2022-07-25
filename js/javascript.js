9e5a1d5720ca27a56dc8c270cf1a5d61d259d760
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2022-07-25 17:13:57 -0300

    Ready to deploy

--- js/javascript.js
+++ js/javascript.js
@@ -59,15 +59,7 @@ d.addEventListener("DOMContentLoaded", () => {
     $blurMenu.classList.toggle("change-to-blured");
   });
 
-  // $blurMenu.addEventListener("click", () => {
-  //   $hamburgerClass.classList.toggle("open");
-  //   $menuClass.classList.toggle("change");
-  //   $circleMenu.classList.toggle("change-bg");
-  //   $blurMenu.classList.toggle("change-to-blured");
-  // });
-
   $blurMenu.addEventListener("touchstart", () => {
-    console.log("touched");
     $hamburgerClass.classList.toggle("open");
     $menuClass.classList.toggle("change");
     $circleMenu.classList.toggle("change-bg");
